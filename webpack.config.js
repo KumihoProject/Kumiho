b440d809cd802ff4ba46b0b006122d7f78eca2d6
rafe.lee <holo@wisewolf.org>
2019-12-15 00:48:27 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = (env) => ({
+    mode: env.prod? 'production' : 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_module/,
+                use:{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+})
